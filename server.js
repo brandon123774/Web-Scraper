6e230121110d25f32800b3f61b4c7af1dd8b1e02
Brandon Shankar <bshankar12@comcast.net>
2020-02-08 20:55:55 -0800

    added server.js info

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ var mongoose = require ("mongoose");
 var axios = require ("axios");
 var cheerio = require ("cheerio");
 
+//db for models
+var db = require ("./models");
+
+//port for localhost
+var PORT = 3000;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// initiate server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
