7ac2a11004cc5db4bda8b6587b55580a62b2d1b4
Brandon Shankar <bshankar12@comcast.net>
2020-02-08 20:49:18 -0800

    added files and folders

---
+++ server.js
@@ -0,0 +1,9 @@
+//dependencies
+var express = require ("express");
+var handlebars = require ("express-handlebars");
+var mongoose = require ("mongoose");
+
+//web scraping tools
+var axios = require ("axios");
+var cheerio = require ("cheerio");
+
