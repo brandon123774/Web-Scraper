9ad5a7c0cd353ba6258d0c89b12859ef8962dad0
Brandon Shankar <bshankar12@comcast.net>
2020-02-09 21:17:17 -0800

    added to server.js file

--- server.js
+++ server.js
@@ -2,13 +2,34 @@
 var express = require ("express");
 var handlebars = require ("express-handlebars");
 var mongoose = require ("mongoose");
+var logger = require ("morgan");
 
 //web scraping tools
 var axios = require ("axios");
 var cheerio = require ("cheerio");
 
+//initialize the application
+var app = express();
+
 //db for models
-var db = require ("./models");
+var config = require ("./config/db");
+mongoose.Promise = Promise;
+mongoose
+  .connect(config.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then( result => {
+    console.log(`Database Connected: '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+  })
+  .catch(err => console.log('Connection error:', err));
+
+//morgan setup 
+app.use(logger('dev'));
+
+//setting up handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //port for localhost
 var PORT = 3000;
@@ -20,6 +41,8 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+//routes
+var index = require("./")
 // initiate server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
