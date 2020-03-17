b2fdf0ed15d4b5c8517e0540f36f8278dc80201e
Brandon Shankar <bshankar12@comcast.net>
2020-03-17 16:38:24 -0700

    fixing allRoutes.js

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 
             var array = []
             console.log("length:", articles.length)
-            var arrayArticles = articles.map(elem=>{
+            var arrayArticles = articles.map(elem => {
                 return {
                     id: elem._id,
                     title: elem.title,
@@ -27,14 +27,15 @@ router.get('/', (req, res) => {
                 }
             })
             console.log(articles)
-            res.render('index', { items: arrayArticles })
+            // console.log(arrayArticles)
+            res.render('index', { items: articles })
         })
         .catch(err => console.log(err));
 });
 
 //get route to root, populating saved.handlebars with articles
 router.get('/saved', (req, res) => {
-    console.log("index route")
+    console.log("saved route")
     db.Article
         .find({})
         .then(articles => {
@@ -44,8 +45,8 @@ router.get('/saved', (req, res) => {
 
             var array = []
             console.log("length:", articles.length)
-            
-            var arrayArticles = articles.map(elem=>{
+
+            var arrayArticles = articles.map(elem => {
                 return {
                     id: elem._id,
                     title: elem.title,
@@ -59,7 +60,7 @@ router.get('/saved', (req, res) => {
         .catch(err => console.log(err));
 });
 
-//test to see if articles are scraped
+// //test to see if articles are scraped
 router.get('/test', (req, res) => {
     console.log("index route")
     db.Article
@@ -72,17 +73,8 @@ router.get('/test', (req, res) => {
         })
         .catch(err => res.json(err));
 });
-//get route to update
-router.put('/articles/save/:id', (req, res) => {
-    console.log("article route")
-    db.Article
-        .updateOne({ _id: req.params.id }, { saved: true })
-        .then(result => res.json(result))
-        .catch(err => res.json(err));
-});
 
 //route to scrape new articles
-
 router.get("/scrape", function (req, res) {
     //options
     console.log("scrape route")
@@ -92,40 +84,106 @@ router.get("/scrape", function (req, res) {
             return cheerio.load(body);
         }
     };
-    axios.get(options.uri).then(function (response) {
-        // console.log(response.data)
-        var $ = cheerio.load(response.data)
+    //calling the db to return articles
+    db.Article
+        .find({})
+        .then((savedArticles) => {
+            //creating an array of saved article headlines            
+            var $ = cheerio.load(savedArticles)
+            // axios call 
+            axios.get(options.uri).then(function (response) {
+                // console.log(response.data)
+                var $ = cheerio.load(response.data)
+                var newArticleArray = []
+                var savedHeadlines = savedArticles.map(article => article.headline);
 
-        var newArticleArray = []
+                //iterating over returned articles, and creating a newArticle object from the data
+                $('.topic-story').each((i, element) => {
+                    //console.log(element)
+                    console.log("--->", $(element).find('img').attr('data-src'))
+                    var newArticle = new db.Article({
+                        storyUrl: $(element).attr("href"),
+                        storyUrl: `https://www.slate.com/technology${$(element).find('a').attr('href')}`,
+                        headline: $(element).find('.topic-story__hed').text().trim(),
+                        // summary: $(element).find('p').text().trim(),
+                        imgUrl: $(element).find('img').attr('data-src'),
+                    });
+                    console.log(newArticle)
+                    //checking to make sure newArticle contains a storyUrl
+                    if (newArticle.storyUrl) {
+                        //checking if new article matches any saved article, 
+                        if (!savedHeadlines.includes(newArticle.headline)) {
+                        newArticleArray.push(newArticle);
+                        }
+                    }
+                });
 
-        //iterating over returned articles, and creating a newArticle object from the data
-        $('.topic-story').each((i, element) => {
-            //console.log(element)
-            console.log("--->", $(element).find('img').attr('data-src'))
-            var newArticle = new db.Article({
-                link: $(element).attr("href"),
-                storyUrl: `https://www.slate.com${$(element).find('a').attr('href')}`,
-                title: $(element).find('.topic-story__hed').text().trim(),
-                summary: $(element).find('p').text().trim(),
-                imgUrl: $(element).find('img').attr('data-src'),
-            });
-            console.log(newArticle)
-            
-            //checking to make sure newArticle contains a storyUrl
-            if (newArticle.link) {
-                newArticleArray.push(newArticle);
-                
-            }
+                //adding all new articles to database
+                console.log(newArticleArray)
+                db.Article
+                    .create(newArticleArray)
+                    .then(result => res.json({ count: newArticleArray.length }))//returning count of new articles to front end
+                    .catch(err => { });
+
+            })
+
         });
-
-        //adding all new articles to database
-        console.log(newArticleArray)
-        db.Article
-            .create(newArticleArray)
-            .then(result => res.json({ count: newArticleArray.length }))//returning count of new articles to front end
-            .catch(err => { });
-
-    })
-
 });
-module.exports = router;
+
+    // //get route to update
+    // router.put('/articles/save/:id', (req, res) => {
+    //     console.log("article route")
+    //     db.Article
+    //         .updateOne({ _id: req.params.id }, { saved: true })
+    //         .then(result => res.json(result))
+    //         .catch(err => res.json(err));
+    // });
+
+    // //route to scrape new articles
+
+    // router.get("/scrape", function (req, res) {
+    //     //options
+    //     console.log("scrape route")
+    //     var options = {
+    //         uri: 'https://slate.com/technology',
+    //         transform: function (body) {
+    //             return cheerio.load(body);
+    //         }
+    //     };
+    //     axios.get(options.uri).then(function (response) {
+    //         // console.log(response.data)
+    //         var $ = cheerio.load(response.data)
+
+    //         var newArticleArray = []
+
+    //         //iterating over returned articles, and creating a newArticle object from the data
+    //         $('.topic-story').each((i, element) => {
+    //             //console.log(element)
+    //             console.log("--->", $(element).find('img').attr('data-src'))
+    //             var newArticle = new db.Article({
+    //                 link: $(element).attr("href"),
+    //                 storyUrl: `https://www.slate.com${$(element).find('a').attr('href')}`,
+    //                 title: $(element).find('.topic-story__hed').text().trim(),
+    //                 summary: $(element).find('p').text().trim(),
+    //                 imgUrl: $(element).find('img').attr('data-src'),
+    //             });
+    //             console.log(newArticle)
+
+    //             //checking to make sure newArticle contains a storyUrl
+    //             if (newArticle.link) {
+    //                 newArticleArray.push(newArticle);
+
+    //             }
+    //         });
+
+    //         //adding all new articles to database
+    //         console.log(newArticleArray)
+    //         db.Article
+    //             .create(newArticleArray)
+    //             .then(result => res.json({ count: newArticleArray.length }))//returning count of new articles to front end
+    //             .catch(err => { });
+
+    //     })
+
+    // });
+    module.exports = router;
